fd25caea9d24a49303751be72942523b7f383876
madison-mitchell <missmadisonrose@icloud.com>
2023-06-10 16:41:57 -0400

    Updated URL for Prod

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfolio-2023/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 })
