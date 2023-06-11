1e09927a40af7291febaca4173edd51d29a27885
Madison Mitchell <madisonrosemitchell@gmail.com>
2023-06-10 23:53:06 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfolio-2023/'
-    : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   configureWebpack: {
     resolve: {
       fallback: {
